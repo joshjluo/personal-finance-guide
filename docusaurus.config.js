1d7bc71ec6770f8bc18667648529bca4bbc9c838
Josh Luo <joshjluo@gmail.com>
2020-11-27 18:32:04 -0600

    feat: deploy docusaurus template to github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Personal Finance Guide',
+  tagline: 'Consolidated information to help you manage your money',
+  url: 'https://joshjluo.github.io',
+  baseUrl: '/personal-finance-guide/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'joshjluo',
+  projectName: 'personal-finance-guide',
   themeConfig: {
     navbar: {
       title: 'My Site',
